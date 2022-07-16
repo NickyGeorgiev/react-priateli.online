7bf5016062ba7de066fa77ddda60e6752378e9b4
Nikolay Georgiev <n.georrgiev@gmail.com>
2022-07-16 19:05:44 +0300

    upload pictures

---
+++ src/components/assets/Pictures.js
@@ -0,0 +1,35 @@
+import salati from './salati.webp'
+import kartofeni from './kartofeni.webp'
+import bezmesni from './bezmesni.webp'
+import pileshkaSkara from './pileshka-skara.webp'
+import svinskaSkara from './svinska-skara.webp'
+import pileshkiMezeta from './pileshki-mezeta.webp'
+import svinskiMezeta from './svinski-mezeta.webp'
+import pileshkiQstiq from './pileshki-qstiq.webp'
+import svinskiQstiq from './svinski-qstiq.webp'
+import sachove from './sachove.webp'
+import spageti from './spageti.webp'
+import rizoto from './rizoto.webp'
+import garnituri from './garnituri.webp'
+import pitki from './pitki.webp'
+import mezeta from './mezeta.webp'
+import deserti from './deserti.webp'
+
+export const pics = {
+    salati,
+    kartofeni,
+    bezmesni,
+    pileshkaSkara,
+    svinskaSkara,
+    pileshkiMezeta,
+    svinskiMezeta,
+    pileshkiQstiq,
+    svinskiQstiq,
+    sachove,
+    spageti,
+    rizoto,
+    garnituri,
+    pitki,
+    mezeta,
+    deserti
+}
